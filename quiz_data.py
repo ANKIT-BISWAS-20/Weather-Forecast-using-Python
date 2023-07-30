aea95765df4b4209e369eca9acc5322c03f9528d
Ankit Biswas <ankit_b.cse2021@msit.edu.in>
2023-07-30 22:44:23 +0530

    first commit

---
+++ quiz_data.py
@@ -0,0 +1,28 @@
+quiz_data = [
+    {
+        "question": "From which part of the world does okra originate?",
+        "choices": ["Africa","China","Southeast Asia","South America"],
+        "answer": "Africa"
+    },
+    {
+        "question": "Which is the biggest continent in the world?",
+        "choices": ["North America","Asia","Africa","Australia"],
+        "answer": "Asia"
+    },
+    {
+        "question": "Which state has the longest coastal line in India?",
+        "choices": ["Gujarat","Kerala","Karnataka","West Bengal"],
+        "answer": "Gujarat"
+    },
+    {
+        "question": "On which river the Uri dam is constructed?",
+        "choices": ["Beas","Jhalam","Sutlej","Ganga"],
+        "answer": "Jhalam"
+    },
+    {
+        "question": "Which is hottest continent on Earth?",
+        "choices": ["Africa","South Asia","North America","Australia"],
+        "answer": "Africa"
+    }
+
+]
